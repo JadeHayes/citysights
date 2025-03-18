4b4699293c2664cc44ea6b17a985fce84356a31a
Jade Hayes <jade.e.hayes@gmail.com>
2025-03-17 20:15:55 -0700

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Jade Hayes on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
